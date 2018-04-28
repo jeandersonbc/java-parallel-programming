cebd90515a141c4f95a07d47bdff3f249238ef7c
Jeanderson Candido <jeandersonbc@gmail.com>
2018-04-28 12:00:43 -0300

    Kickoff demo

---
+++ src/main/java/gh/jbc/demo/pp/App.java
@@ -0,0 +1,13 @@
+package gh.jbc.demo.pp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
